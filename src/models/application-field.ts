d7a0f72796f1f61435bc08f83a2bfcaa33e7a806
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-07-14 07:45:51 -0400

    Adding in new fields for label and name in application field

--- src/models/application-field.ts
+++ src/models/application-field.ts
@@ -5,16 +5,22 @@ export class ApplicationField {
   applicationFieldId: string;
   applicationSequence: number;
   inputFieldType: InputFieldType;
+  inputFieldLabel: string;
+  inputFieldName: string;
 
   constructor(options: {
     applicationId: string;
     applicationFieldId: string;
     applicationSequence?: number;
     inputFieldType?: InputFieldType;
+    inputFieldLabel?: string;
+    inputFieldName?: string;
   }) {
     this.applicationId = options.applicationId;
     this.applicationFieldId = options.applicationFieldId;
     this.applicationSequence = options.applicationSequence;
     this.inputFieldType = options.inputFieldType;
+    this.inputFieldLabel = options.inputFieldLabel;
+    this.inputFieldName = options.inputFieldName;
   }
 }
