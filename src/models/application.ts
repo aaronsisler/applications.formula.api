62b35ae21a556c582b99f7c52a11c3cce275aaf9
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-07-31 12:49:06 -0400

    Pushing change for tenantId

--- src/models/application.ts
+++ src/models/application.ts
@@ -3,17 +3,20 @@ import { ApplicationField } from "./application-field";
 
 export class Application {
   applicationId: string;
+  tenantId: string;
   applicationName: string;
   applicationFields: ApplicationField[];
   applicants: ApplicationApplicant[];
 
   constructor(options: {
     applicationId?: string;
+    tenantId?: string;
     applicationName?: string;
     applicationFields?: ApplicationField[];
     applicants?: ApplicationApplicant[];
   }) {
     this.applicationId = options.applicationId;
+    this.tenantId = options.tenantId;
     this.applicationName = options.applicationName;
     this.applicationFields = options.applicationFields;
     this.applicants = options.applicants;
